24ed529521cbb7550f286aca99a4abe6576f4f14
Iftekar Patel <iftekarpatel@gmail.com>
2026-07-01 23:19:51 +0530

    added division and mul in app.py to create merge conflict with area branch

--- app.py
+++ app.py
@@ -1,11 +1,13 @@
-from calc_func import add, subtract
+from calc_func import add, subtract, divide,multiply
 
 def main():
     print("""Select the function from the given options:
 1. Add
-2. Subtract""")
+2. Subtract
+3. Multiply
+4. Divide""")
     
-    choice = input("Select the function (1 or 2): ")
+    choice = input("Select the function (1, 2, 3, or 4): ")
     
     if choice == "1":
         a = float(input("Enter the first number: "))
@@ -15,8 +17,16 @@ def main():
         a = float(input("Enter the first number: "))
         b = float(input("Enter the second number: "))
         print(f"The result of subtraction is: {subtract(a, b)}")
+    elif choice == "3":
+        a = float(input("Enter the first number: "))
+        b = float(input("Enter the second number: "))
+        print(f"The result of multiplication is: {multiply(a, b)}")
+    elif choice == "4":
+        a = float(input("Enter the first number: "))
+        b = float(input("Enter the second number: "))
+        print(f"The result of division is: {divide(a, b)}")
     else:
-        print("Invalid choice. Please select 1 or 2.")
+        print("Invalid choice. Please select 1, 2, 3, or 4.")
 
 if __name__ == "__main__":
     main()
